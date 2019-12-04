8bed9dc3fc3b6c7eafaa06d54e1464980d6558e4
zzzz王超zzzz <wangchao7@yiche.com>
2019-12-04 09:44:35 +0800

    11

---
+++ BHCustomLibrary/Classes/SayMessage.h
@@ -0,0 +1,19 @@
+//
+//  SayMessage.h
+//  TestLib
+//
+//  Created by wangchao on 2019/12/3.
+//  Copyright © 2019 wangchao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SayMessage : NSObject
+- (void)sayHi;
+- (void)sayBye;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BHCustomLibrary/Classes/SayMessage.m
@@ -0,0 +1,22 @@
+//
+//  SayMessage.m
+//  TestLib
+//
+//  Created by wangchao on 2019/12/3.
+//  Copyright © 2019 wangchao. All rights reserved.
+//
+
+#import "SayMessage.h"
+
+@implementation SayMessage
+- (void)sayHi
+{
+    NSLog(@"say  hi");
+}
+
+- (void)sayBye
+{
+        NSLog(@"say  bye");
+
+}
+@end
